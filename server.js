5a223794e4f641354f6fa256b836fcaff1e7df4c
abd20ac <114482939+abd20ac@users.noreply.github.com>
2023-02-13 08:52:02 +0000

    lmao les go circles!!!

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const fs = require("fs");
+const app = express();
 
+app.get('/', (req, res) => {
+    res.sendFile('./index.html', {root: __dirname});
+})
+
+app.listen(3000);
